f52433084fc3e553cfdd45185c6bb2f7997e2d4a
Erhan <e.batu44@icloud.com>
2023-06-10 22:36:27 +0200

    197. RestTemplateBuilder Configuration

--- src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
+++ src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
@@ -18,7 +18,10 @@ public class BeerClientImpl implements BeerClient {
 
     private final RestTemplateBuilder restTemplateBuilder;
 
-    private static final String BASE_URL = "http://localhost:8080";
+    //I already implement this in the config file as default you don't need
+//    private static final String BASE_URL = "http://localhost:8080";
+
+    //with the different environment you should only change PATH
     private static final String GET_BEER_PATH = "/api/v1/beer";
 
     @Override
@@ -28,17 +31,17 @@ public class BeerClientImpl implements BeerClient {
 
         //ResponseEntity<String> I made this because I will take JSON which is String
         ResponseEntity<String> stringResponse =
-                restTemplate.getForEntity(BASE_URL+GET_BEER_PATH, String.class);
+                restTemplate.getForEntity(GET_BEER_PATH, String.class);
 
         //We invoke here Jackson and it will convert JSON to MAP
         ResponseEntity<Map> mapResponse =
-                restTemplate.getForEntity(BASE_URL+GET_BEER_PATH, Map.class);
+                restTemplate.getForEntity(GET_BEER_PATH, Map.class);
 
-        ResponseEntity<JsonNode> jsonResponse = restTemplate.getForEntity(BASE_URL+GET_BEER_PATH, JsonNode.class);
+        ResponseEntity<JsonNode> jsonResponse = restTemplate.getForEntity(GET_BEER_PATH, JsonNode.class);
 
         //First this will go to beers and after beers it will do iteration and bring you all beerNames
         //This is Jackson
-        jsonResponse.getBody().findPath("beers")
+        jsonResponse.getBody().findPath("content")
                         .elements().forEachRemaining(node -> {
                     System.out.println(node.get("beerName").asText());
                 });
@@ -54,7 +57,7 @@ public class BeerClientImpl implements BeerClient {
         RestTemplate restTemplate = restTemplateBuilder.build();
 
         //this is gonna convert JSON from API to my model in BeerDTO
-        ResponseEntity<BeerDTOPageImpl> response = restTemplate.getForEntity(BASE_URL+GET_BEER_PATH, BeerDTOPageImpl.class);
+        ResponseEntity<BeerDTOPageImpl> response = restTemplate.getForEntity(GET_BEER_PATH, BeerDTOPageImpl.class);
 
 
 
