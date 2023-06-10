02ba3ca0f8ad0b50b22ef55a4e573d4e3e0d6370
Erhan <e.batu44@icloud.com>
2023-06-10 20:25:54 +0200

    195. Get List as Jackson Object

--- src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
+++ src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
@@ -1,5 +1,6 @@
 package guru.springframework.spring6resttemplate.client;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import guru.springframework.spring6resttemplate.model.BeerDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.web.client.RestTemplateBuilder;
@@ -16,8 +17,8 @@ public class BeerClientImpl implements BeerClient {
 
     private final RestTemplateBuilder restTemplateBuilder;
 
-    public static final String BASE_URL = "http://localhost:8080";
-    public static final String GET_BEER_PATH = "/api/v1/beer";
+    private static final String BASE_URL = "http://localhost:8080";
+    private static final String GET_BEER_PATH = "/api/v1/beer";
 
     @Override
     public Page<BeerDTO> listBeers() {
@@ -31,6 +32,15 @@ public class BeerClientImpl implements BeerClient {
         //We invoke here Jackson and it will convert JSON to MAP
         ResponseEntity<Map> mapResponse =
                 restTemplate.getForEntity(BASE_URL+GET_BEER_PATH, Map.class);
+
+        ResponseEntity<JsonNode> jsonResponse = restTemplate.getForEntity(BASE_URL+GET_BEER_PATH, JsonNode.class);
+
+        //First this will go to beers and after beers it will do iteration and bring you all beerNames
+        //This is Jackson
+        jsonResponse.getBody().findPath("beers")
+                        .elements().forEachRemaining(node -> {
+                    System.out.println(node.get("beerName").asText());
+                });
 
         System.out.println(stringResponse.getBody());
 
