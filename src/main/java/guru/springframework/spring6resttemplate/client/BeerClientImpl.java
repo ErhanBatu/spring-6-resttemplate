f8cd46e8831e0c953488467b493719da6305507a
Erhan <e.batu44@icloud.com>
2023-06-14 00:52:07 +0200

    199. Uri Components Builder

--- src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
+++ src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.Map;
 
@@ -58,9 +59,13 @@ public class BeerClientImpl implements BeerClient {
 
         RestTemplate restTemplate = restTemplateBuilder.build();
 
+        //UriComponentsBuilder ALLOWS US TO PUT QUERY PARAM. fromPath MEANS YOU GET THE DEFAULT PATH AND YOU PUT QUERY ON IT
+        //YOU ARE NOT DEFINING NEW PATH HERE
+        UriComponentsBuilder uriComponentsBuilder = UriComponentsBuilder.fromPath(GET_BEER_PATH);
+
         //we are converting from JSON directly to our POJO
         ResponseEntity<BeerDTOPageImpl> response =
-                restTemplate.getForEntity(GET_BEER_PATH, BeerDTOPageImpl.class);
+                restTemplate.getForEntity(uriComponentsBuilder.toUriString(), BeerDTOPageImpl.class);
 
         return response.getBody();
     }
