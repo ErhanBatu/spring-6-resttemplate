fb4fbfe4e2c2abd1cca0366d7be24ca49271d49b
Erhan <e.batu44@icloud.com>
2023-06-14 02:32:48 +0200

    208. Using Mockito

--- src/test/java/guru/springframework/spring6resttemplate/client/BeerClientMockTest.java
+++ src/test/java/guru/springframework/spring6resttemplate/client/BeerClientMockTest.java
@@ -2,41 +2,64 @@ package guru.springframework.spring6resttemplate.client;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import guru.springframework.spring6resttemplate.config.RestTemplateBuilderConfig;
 import guru.springframework.spring6resttemplate.model.BeerDTO;
 import guru.springframework.spring6resttemplate.model.BeerDTOPageImpl;
 import guru.springframework.spring6resttemplate.model.BeerStyle;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
+import org.springframework.boot.test.web.client.MockServerRestTemplateCustomizer;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.client.MockRestServiceServer;
+import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.UUID;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
 import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
 
 //WHY YOU NEED MOCK TEST BECAUSE IN THE REAL TEST (BeerClientImplTest) YOU HAVE TO USE REAL DATA AND END POINT
-@RestClientTest(BeerClientImpl.class)
+@RestClientTest
+@Import(RestTemplateBuilderConfig.class)
 public class BeerClientMockTest {
 
     static final String URL = "http://localhost:8080";
 
-    @Autowired
     BeerClient beerClient;
 
-    @Autowired
     MockRestServiceServer server;
+
+    @Autowired
+    RestTemplateBuilder restTemplateBuilderConfigure;
 
     //I will return JSON payload with this object
     @Autowired
     ObjectMapper objectMapper;
+
+    @Mock
+    RestTemplateBuilder mockRestTemplateBuilder = new RestTemplateBuilder(new MockServerRestTemplateCustomizer());
+
+    @BeforeEach
+    void setUp() {
+        RestTemplate restTemplate = restTemplateBuilderConfigure.build();
+        //I initiliaze here new server, I am not using Spring server, basically mimicking
+        server = MockRestServiceServer.bindTo(restTemplate).build();
+        //i bound the resttemplate to server
+        when(mockRestTemplateBuilder.build()).thenReturn(restTemplate);
+        beerClient = new BeerClientImpl(mockRestTemplateBuilder);
+    }
 
     @Test
     void testListBeers() throws JsonProcessingException {
